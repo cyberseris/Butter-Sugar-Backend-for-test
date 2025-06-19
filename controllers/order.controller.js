9e96ba436cca8215e30f02db35112053e0ded453
cyberseris <zihlingchen@gmail.com>
2025-06-20 04:02:38 +0800

    取得單筆訂單

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -35,9 +35,6 @@ const orderController = {
     async getOrder(req, res, next){
         const user_id = req.user.id
         const order_number = req.params.orderNumber
-        console.log("==========getOrder=========")
-        console.log(order_number)
-        console.log("==========getOrder=========")
         
         const orderItemRepo = dataSource.getRepository('order_item')
         const result = await orderItemRepo.createQueryBuilder('orderItem')
@@ -45,12 +42,14 @@ const orderController = {
             'order.order_number AS order_number', 
             'array_agg(course.course_name) AS course_name', 
             'order.final_amount AS final_amount', 
+            'order.pay_rtn_msg AS pay_rtn_msg', 
             'order.created_at AS created_at'])
         .leftJoin('orderItem.order', 'order')
         .leftJoin('orderItem.courses', 'course')
         .where('order.user_id = :user_id AND order.payment_status = :status AND order.order_number = :order_number', {user_id: user_id, status: 'paid', order_number: order_number})
         .groupBy('order.order_number')
         .addGroupBy('order.final_amount')
+        .addGroupBy('order.pay_rtn_msg')
         .addGroupBy('order.created_at')
         .getRawMany()
 
