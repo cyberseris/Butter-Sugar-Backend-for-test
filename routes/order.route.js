b908edf51654d7f8a25bbecb51d189491a8f5d43
cyberseris <zihlingchen@gmail.com>
2025-06-20 04:51:24 +0800

    修改 order 路由

--- routes/order.route.js
+++ routes/order.route.js
@@ -4,9 +4,9 @@ const orderController = require('../controllers/order.controller')
 const isAuth = require('../middleware/isAuth.middleware')
 const handleMiddleware = require('../utils/handleMiddleware')
 
-// 取得所有訂單
+/* // 取得所有訂單
 router.get('/', ...handleMiddleware([isAuth], orderController.getOrderList))
 // 取得所有訂單
-router.get('/:orderNumber', ...handleMiddleware([isAuth], orderController.getOrder))
+router.get('/:orderNumber', ...handleMiddleware([isAuth], orderController.getOrder)) */
 
 module.exports = router
--- routes/users.route.js
+++ routes/users.route.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const passport = require('passport')
 const userController = require('../controllers/user.controller')
+const orderController = require('../controllers/order.controller')
 const isAuth = require('../middleware/isAuth.middleware')
 const validateSchema = require('../middleware/validateSchema.middleware')
 const { updateUserSchema } = require('../schema/user.schema')
@@ -47,9 +48,9 @@ router.get('/check', ...handleMiddleware([isAuth], userController.getCheck))
 ) */
 
 
-/* // 取得所有訂單
+// 取得所有訂單
 router.get('/orders', ...handleMiddleware([isAuth], orderController.getOrderList))
 // 取得所有訂單
-router.get('/orders/:orderNumber', ...handleMiddleware([isAuth], orderController.getOrder)) */
+router.get('/orders/:orderNumber', ...handleMiddleware([isAuth], orderController.getOrder))
 
 module.exports = router
