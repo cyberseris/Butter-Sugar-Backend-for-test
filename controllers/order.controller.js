6fb6894ee42694053e301fd45180f48bfb8a45b7
cyberseris <zihlingchen@gmail.com>
2025-06-20 03:35:51 +0800

    取得單筆訂單

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -26,6 +26,32 @@ const orderController = {
         .getRawMany()
 
         return sendResponse(res, 200, true, '成功取得訂單', result)
+    },
+
+    /*
+    * 取得單筆訂單
+    * @route PATCH - /api/v1/users/order/:order-number
+    */
+    async getOrder(req, res, next){
+        const user_id = req.user.id
+        const order_number = req.params.order_number
+        
+        const orderItemRepo = dataSource.getRepository('order_item')
+        const result = await orderItemRepo.createQueryBuilder('orderItem')
+        .select([
+            'order.order_number AS order_number', 
+            'array_agg(course.course_name) AS course_name', 
+            'order.final_amount AS final_amount', 
+            'order.created_at AS created_at'])
+        .leftJoin('orderItem.order', 'order')
+        .leftJoin('orderItem.courses', 'course')
+        .where('order.user_id = :user_id AND order.payment_status = :status AND order.order_number = :order_number', {user_id: user_id, status: 'paid', order_number: order_number})
+        .groupBy('order.order_number')
+        .addGroupBy('order.final_amount')
+        .addGroupBy('order.created_at')
+        .getRawMany()
+
+        return sendResponse(res, 200, true, '成功取得訂單', result)
     }
 }
 
